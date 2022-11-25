d8e10f357795e594fea0d4757f80969cdf806d0a
Julien Tremblay <jtrembla@gra-login1.graham.sharcnet>
2022-11-25 16:19:13 -0500

    added post

---
+++ _posts/2021-05-21-Interactive animated stacked bar plot in D3.js
@@ -0,0 +1,14 @@
+---
+layout: post
+category : Data vizualization
+tagline: ""
+title: "Interactive sortable stacked bar plot in D3.js"
+description: "Implementation of a sortable interactive taxonomic profile stacked barchart in D3.js"
+tags : [D3, D3.js, stacked barcharts, stacked barplots, taxonomic profiles]
+---
+{% include JB/setup %}
+
+### Fun sortable stacked barcharts in D3
+Have a look at my awesome [taxonomic profiles interactive stacked barcharts](https://observablehq.com/@jtremblay/stacked-bar-chart). 
+My next objective is to implement a multi-panel sortable stacked barchart. Stay put. 
+
